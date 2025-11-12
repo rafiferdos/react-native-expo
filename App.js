550b62e91f81d3b4d0f6ab64709d615cdeb31323
rafiferdos <rafiferdos@gmail.com>
2025-11-12 15:17:14 +0600

    Add jsconfig.json for TypeScript configuration with strict settings

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        source={require("./assets/licensed-image.jpg")}
+        style={{ width: 100, height: 100, borderRadius: 50 }}
+      />
       <Text style={{ color: "violet", fontSize: 45 }}>Hi there!</Text>
-      <StatusBar style='auto' />
+      <StatusBar style="" />
     </View>
   );
 }
